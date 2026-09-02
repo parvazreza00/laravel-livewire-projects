8cda7627d44e342306032e46a8d72e5571a2afd9
parvazreza00 <parvazreza00@gmail.com>
2026-09-02 12:12:38 +0600

    filtering by column name wise(table header) ascending or descengin and fontawesome icon added

--- livewire-crud-one/resources/views/layouts/app.blade.php
+++ livewire-crud-one/resources/views/layouts/app.blade.php
@@ -5,6 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <title>{{ $title ?? config('app.name') }}</title>
+        {{-- fontawesoem icons --}}
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.3.1/css/all.min.css" integrity="sha512-QeR2VH+lsBE5LSAe1Q5EnTBbe7XTBubt8dG93Y7gidSgdMCr8nVqKcfKAMyN96SV8KDbZVTDXChatu5G2KQGzg==" crossorigin="anonymous" referrerpolicy="no-referrer">
 
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
@@ -15,6 +17,10 @@
                 vertical-align: middle;
                 border:1px solid gray;
                 text-align: center
+            }
+
+            tr th span{
+                cursor: pointer;
             }
         </style>
     </head>
